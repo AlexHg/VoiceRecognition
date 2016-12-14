5fc5bce56a07d5c07978ab037ace177672d88b8d
Joanna <fickle.joan@gmail.com>
2016-12-14 11:30:49 +0100

    Mel centers calculation

--- src/melScale.m
+++ src/melScale.m
@@ -1,2 +1,15 @@
-melHzScaleBands = [160; 394; 670;1000;1420;1900;2450;3120;400;5100;6600];
-melcenters(1) = 160;
+melHzScaleBands = [160; 394; 670; 1000; 1420; 1900; 2450; 3120; 400; 5100; 6600];
+% melcenters(1) = 160;
+
+melcenters = [];
+melcenters(1) = 0;
+scaleLeng = length(melHzScaleBands);
+for i = 1 : scaleLeng-1
+    diff = (melHzScaleBands(i+1) - melHzScaleBands(i))/2;
+    melcenters(i+1) = melHzScaleBands(i) + diff;
+end
+
+diff32 = melcenters(3) - melcenters(2);
+melcenters(1) = melcenters(2) - diff32;
+diffEnd = melcenters(scaleLeng) - melcenters(scaleLeng-1);
+melcenters(scaleLeng + 1) = melcenters(scaleLeng) + diffEnd;
