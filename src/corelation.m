d1eefbde2750b5e6f57f094fac65b3ba7d559ed0
Kiedro <kiedro@outlook.com>
2016-12-15 00:15:54 +0100

    Coleration, .mat data

---
+++ src/corelation.m
@@ -0,0 +1,27 @@
+% zwraca wspó³czynnik korelacji pomiêdzy macierzami o dowolnych rozmiarach
+function [result] = corelation(a, b)
+
+aSize = size(a);
+bSize = size(b);
+
+if(aSize(1) ~= bSize(1))
+    if(aSize(1) > bSize(1))
+        paddBy = [aSize(1)-bSize(1) 0];
+        b = padarray(b,paddBy,'post');
+    else
+        paddBy = [bSize(1)-aSize(1) 0];
+        a = padarray(a,paddBy,'post');
+    end
+end
+
+if(aSize(2) ~= bSize(2))
+    if(aSize(2) > bSize(2))
+        paddBy = [0 aSize(2)-bSize(2)];
+        b = padarray(b,paddBy,'post');
+    else
+        paddBy = [0 bSize(2)-aSize(2)];
+        a = padarray(a,paddBy,'post');
+    end
+end
+result = corrcoef(a,b);
+result = result(1,2);
--- src/main.m
+++ src/main.m
@@ -4,21 +4,21 @@ clear figure;
 
 %% 3-5 s³ów nagrane 9-krotnie
 filesCount = 9;
-% fileNameBegin = 'data/ksiazka/ksiazka';
-% fileNameBegin = 'data/krzeslo/krzeslo';
- fileNameBegin = 'data/fotel/fotel';
-
+  fileNameBegin = 'data/ksiazka/ksiazka';
+ %fileNameBegin = 'data/krzeslo/krzeslo';
+ %fileNameBegin = 'data/fotel/fotel';
+coeffs = cell(1,filesCount);
 for i = 1:filesCount
     filename = strcat(fileNameBegin, num2str(i), '.wav');
     %% Wspolczynniki po banku filtrow dla 1 pliku
     dtPart = 0.1;
     fftPoints = 128;
     filtersBankLength = 10;
-    coeffs = getCoeffs(filename, dtPart);
+    coeffs{i} = getCoeffs(filename, dtPart);
     
  %   subplot(1,2,1), subimage(imagesc(coeffs))
-   subplot(3,3,i)
-   imagesc(coeffs);
+    subplot(3,3,i)
+    imagesc(coeffs{i});
 end
 %axis([0 7000 0 4]) 
 
