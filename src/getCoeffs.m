0b3388dfb7402870dace2f7ad2bbcfcde7da35c9
Kiedro <kiedro@outlook.com>
2016-12-14 11:07:00 +0100

    FFt changes, mel scale init

--- src/getCoeffs.m
+++ src/getCoeffs.m
@@ -15,9 +15,15 @@ coeffs = [];
 while (I0 + samplePartCount <= totalSampleCount)
     Iend = I0 + samplePartCount;
     part = signal(I0:Iend);
-
+    L  = length(part);
     %% Fourier analysys    
-    fftResult = abs(fft(part, fftPoints))/length(part);
+    fftResult = fft(part);
+    
+    P2 = abs(fftResult/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    plot(f,P1);
     
     %% Bank filtrów - 8-12 filtrów, w czêstotliwoœciach mowy (5-8kHz - do sprawdzenia)
     coeffs(:,i) = triangleFilter(fftResult, filtersBankLength);
